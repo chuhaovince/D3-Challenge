1aeaa98655e129a9d33139dd21835b9f829423a2
chuhaovince <53956182+chuhaovince@users.noreply.github.com>
2020-01-30 22:21:07 -0500

    add x and y axis. parsed data to numers instead of strings. set margin, defined svg size, added groups "g". defined linear scale functions

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,22 +1,141 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
 
-// set the promise as a const variable
-d3.csv("../assets/data/data.csv").then(function(data){
-    var abbr = data.map(d => d.abbr);
-    var poverty = data.map(d => d.poverty);
-    var age = data.map(d => d.age);
-    var income = data.map(d => d.income);
-    var healthCare = data.map(d => d.healthcare);
-    var obese = data.map(d => d.obesity);
+var margin = {
+  top: 50,
+  right: 50,
+  bottom: 60,
+  left: 50
+};
 
-    trace1 = {
-        x : poverty,
-        y : healthCare,
-        type : "scatter"
-    };
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
 
-    plotdata = [trace1];
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
-    Plotly.newPlot("#scatter",plotdata);
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "hair_length";
+var chosenYAxis = "hair_length";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(stateData, chosenXAxis = "poverty") {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(stateData, d => d[chosenXAxis]) * 0.8,
+      d3.max(stateData, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
+  return xLinearScale;
+};
+
+// function used for updating y-scale var upon click on axis label
+function yScale(stateData, chosenYAxis = "healthcare") {
+  // create scales
+  var yLinearScale = d3.scaleLinear()
+    .domain([d3.min(stateData, d => d[chosenYAxis]) * 0.8,
+      d3.max(stateData, d => d[chosenYAxis]) * 1.2
+    ])
+    .range([height, 0]);
+  return yLinearScale;
+};
+
+// function used for updating xAxis var upon click on axis label
+function renderxAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+  return xAxis;
+};
+
+// function used for updating yAxis var upon click on axis label
+function renderyAxes(newYScale, yAxis) {
+  var leftAxis = d3.axisLeft(newYScale);
+  yAxis.transition()
+    .duration(1000)
+    .call(leftAxis);
+  return yAxis;
+};
+
+// function used for updating circles group with new tooltip
+function updateToolTip(chosenXAxis, circlesGroup) {
+  if (chosenXAxis === "poverty") {
+    var label = "In Poverty (%)";
+  }
+  else {
+    var label = "# of Albums:";
+  }
+
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    //.offset([80, -60])
+    .html(function(d) {
+      return (`${d.rockband}<br>${label} ${d[chosenXAxis]}`);
+    });
+
+  circlesGroup.call(toolTip);
+
+  circlesGroup.on("mouseover", function(data) {
+    toolTip.show(data, this);
+  })
+    // onmouseout event
+    .on("mouseout", function(data) {
+      toolTip.hide(data);
+    });
+
+  return circlesGroup;
+};
+
+// Import data
+d3.csv("../assets/data/data.csv").then(function(stateData){
+  // parse data as number
+  stateData.forEach(function(data) {
+  data.poverty = +data.poverty;
+  data.age = +data.age;
+  data.income = +data.income;
+  data.healtycare = +data.healtycare;
+  data.obesity = +data.obesity;
+  data.smokes = +data.smokes;
+  });
+
+  // xLinearScale function above csv import
+  var xLinearScale = xScale(stateData);
+
+  // yLinearScale function above csv import
+  var xLinearScale = yScale(stateData, 'healthcare');
+
+  // Create initial axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // append x axis
+  var xAxis = chartGroup.append("g")
+  .classed("x-axis", true)
+  .attr("transform", `translate(0, ${height})`)
+  .call(bottomAxis);
+
+  // append y axis
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // Create initial scale functions
+  var xScale = d3.scaleLinear()
+              .domain([20, d3.max(stateData, d => d.hair_length)])
+              .range([0, width]);
+  
+  var ySale = d3.scaleLinear()
+              .domain([0, d3.max(statData, d => d.num_hits)])
+              .range([height, 0]);
+
+
+
 })
 
